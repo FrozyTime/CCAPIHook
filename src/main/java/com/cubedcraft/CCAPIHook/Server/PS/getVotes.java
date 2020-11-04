a43b0b14ba9cb4ad2ce4e27c17e84ab1ea60063a
Tech <debitcardzpaypal@gmail.com>
2020-11-03 21:05:42 -0500

    Get specific PS votes

--- src/main/java/com/cubedcraft/CCAPIHook/Server/PS/getVotes.java
+++ src/main/java/com/cubedcraft/CCAPIHook/Server/PS/getVotes.java
@@ -1,2 +1,23 @@
-package com.cubedcraft.CCAPIHook.server.ps;public class getVotes {
+package com.cubedcraft.CCAPIHook.Server.PS;
+
+import com.cubedcraft.CCAPIHook.Exceptions.BadRequestException;
+import com.cubedcraft.CCAPIHook.HTTPRequest;
+
+import java.io.IOException;
+
+public class getVotes {
+
+    /** Get the votes from the specified server.
+     * @param server String: Server name
+     */
+    public static String getVotes(String server) throws BadRequestException {
+
+        String data;
+        try {
+            data = HTTPRequest.getData("/server/"+server);
+        } catch(IOException ignored) { throw new BadRequestException(); }
+
+        return "xd";
+    }
+
 }
