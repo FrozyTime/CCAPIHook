10ae526fce2d07fdab0fa9de30e2a10eefa224bd
Tech <debitcardzpaypal@gmail.com>
2020-11-03 21:14:14 -0500

    Descriptions

--- src/main/java/com/cubedcraft/CCAPIHook/EZJSONFetchData.java
+++ src/main/java/com/cubedcraft/CCAPIHook/EZJSONFetchData.java
@@ -11,18 +11,30 @@ public class EZJSONFetchData {
         if(CCAPIHook.isDebugging()) System.out.println("Parsing data, " + this.data);
     }
 
+    /** Fetch the votes of the specified server.
+     * @param data String: JSON
+     */
     public static String fetchPSVotes(String data) {
         return "-1";
     }
 
+    /** Fetch the boosters of the specified server.
+     * @param data String: JSON
+     */
     public static String fetchPSBoosters(String data) {
         return "-1";
     }
 
+    /** Fetch all of the top 10 most voted servers by index.
+     * @param index int: Server placement
+     */
     public static String fetchTopVotedServer(int index) {
         return "-1";
     }
 
+    /** Fetch all of the top 10 most boosted servers by index.
+     * @param index int: Server placement
+     */
     public static String fetchTopBoostedServer(int index) {
         return "-1";
     }
